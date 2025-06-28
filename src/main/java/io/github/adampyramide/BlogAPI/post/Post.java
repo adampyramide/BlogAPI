66ccb33e3afc577e38664889c97c6010026b316f
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-06-28 23:26:24 +0200

    Create Post entity and DTO

---
+++ src/main/java/io/github/adampyramide/BlogAPI/post/Post.java
@@ -0,0 +1,27 @@
+package io.github.adampyramide.BlogAPI.post;
+
+import io.github.adampyramide.BlogAPI.user.User;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String title;
+    private String content;
+    private LocalDateTime createTime;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User author;
+
+}
