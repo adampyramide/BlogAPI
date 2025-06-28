bdbae141e9b5efadb6b37f16d79724bdd2ae824f
Adam El Zahiri <adamxelz@gmail.com>
2025-06-28 19:30:42 +0200

    Set up Spring Security

---
+++ src/main/java/io/github/adampyramide/BlogAPI/security/MyUserDetailsService.java
@@ -0,0 +1,24 @@
+package io.github.adampyramide.BlogAPI.security;
+
+import io.github.adampyramide.BlogAPI.user.User;
+import io.github.adampyramide.BlogAPI.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepo;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new UserPrincipal(user);
+    }
+}
