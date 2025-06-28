addd68af3226fce287d4ac3b1771b12aa2f14f9c
Adam El Zahiri <adamxelz@gmail.com>
2025-06-28 23:02:44 +0200

    Remove boilerplate code using Lombok

--- src/main/java/io/github/adampyramide/BlogAPI/user/User.java
+++ src/main/java/io/github/adampyramide/BlogAPI/user/User.java
@@ -1,8 +1,12 @@
 package io.github.adampyramide.BlogAPI.user;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class User {
 
     @Id
@@ -14,31 +18,5 @@ public class User {
 
     @Column(nullable = false)
     private String password;
-
-    public User() {}
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
 }
