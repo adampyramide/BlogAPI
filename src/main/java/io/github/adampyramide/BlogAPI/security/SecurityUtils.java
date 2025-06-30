b55af895008fd5f0081c3bc1f91be0601ae8513e
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-06-30 19:48:48 +0200

    Add securityUtils for fetching authenticated user from request

---
+++ src/main/java/io/github/adampyramide/BlogAPI/security/SecurityUtils.java
@@ -0,0 +1,25 @@
+package io.github.adampyramide.BlogAPI.security;
+
+import io.github.adampyramide.BlogAPI.exception.ApiRequestException;
+import io.github.adampyramide.BlogAPI.user.User;
+import io.github.adampyramide.BlogAPI.user.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityUtils {
+
+    private final UserRepository userRepo;
+
+    public SecurityUtils(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    public User getAuthenticatedUser() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userRepo.findByUsername(username)
+                .orElseThrow(() -> new ApiRequestException("Authenticated user not found", HttpStatus.NOT_FOUND));
+    }
+
+}
