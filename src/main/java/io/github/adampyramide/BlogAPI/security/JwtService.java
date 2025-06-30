a7d3c03336d40df9520e0db6e15783d8e4e7aa23
Adam El Zahiri <adamxelz@gmail.com>
2025-06-30 13:56:34 +0200

    Rename JWTService to JwtService

--- src/main/java/io/github/adampyramide/BlogAPI/security/JwtService.java
+++ src/main/java/io/github/adampyramide/BlogAPI/security/JwtService.java
@@ -17,11 +17,11 @@ import java.util.Map;
 import java.util.function.Function;
 
 @Service
-public class JWTService {
+public class JwtService {
 
     private String secretkey = "";
 
-    public JWTService() {
+    public JwtService() {
 
         try {
             KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
