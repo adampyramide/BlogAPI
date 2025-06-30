2063961eac80c6ada8b76eb6a353f808b78bbd6e
Adam El Zahiri <adamxelz@gmail.com>
2025-06-30 19:47:56 +0200

    Change built-in exception to ApiRequestException

--- src/main/java/io/github/adampyramide/BlogAPI/security/MyUserDetailsService.java
+++ src/main/java/io/github/adampyramide/BlogAPI/security/MyUserDetailsService.java
@@ -1,8 +1,10 @@
 package io.github.adampyramide.BlogAPI.security;
 
+import io.github.adampyramide.BlogAPI.exception.ApiRequestException;
 import io.github.adampyramide.BlogAPI.user.User;
 import io.github.adampyramide.BlogAPI.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -18,7 +20,7 @@ public class MyUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepo.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+                .orElseThrow(() -> new ApiRequestException("User not found", HttpStatus.NOT_FOUND));
         return new UserPrincipal(user);
     }
 }
