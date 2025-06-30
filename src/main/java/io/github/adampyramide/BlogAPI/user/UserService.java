44c542796492e086aa3eab5d1600e788a91c5f92
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-06-30 14:03:27 +0200

    Rename JWTService to JwtService

--- src/main/java/io/github/adampyramide/BlogAPI/user/UserService.java
+++ src/main/java/io/github/adampyramide/BlogAPI/user/UserService.java
@@ -1,29 +1,24 @@
 package io.github.adampyramide.BlogAPI.user;
 
 import io.github.adampyramide.BlogAPI.exception.ApiRequestException;
-import io.github.adampyramide.BlogAPI.security.JWTService;
-import org.slf4j.LoggerFactory;
+import io.github.adampyramide.BlogAPI.security.JwtService;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.logging.Logger;
 
 @Service
 public class UserService {
 
     private final UserRepository repo;
     private final AuthenticationManager authManager;
-    private final JWTService jwtService;
+    private final JwtService jwtService;
 
     private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(12);
 
-    public UserService(UserRepository repo, AuthenticationManager authManager, JWTService jwtService) {
+    public UserService(UserRepository repo, AuthenticationManager authManager, JwtService jwtService) {
         this.repo = repo;
         this.authManager = authManager;
         this.jwtService = jwtService;
