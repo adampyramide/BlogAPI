36f3ce3112ad9a21b57750a5e74b6c86d0cf0da4
Adam El Zahiri <adamxelz@gmail.com>
2025-06-30 20:50:43 +0200

    Set token expiration to 1 day

--- src/main/java/io/github/adampyramide/BlogAPI/security/JwtService.java
+++ src/main/java/io/github/adampyramide/BlogAPI/security/JwtService.java
@@ -39,7 +39,7 @@ public class JwtService {
                 .add(claims)
                 .subject(username)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 60 * 60 * 30))
+                .expiration(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000))
                 .and()
                 .signWith(getKey())
                 .compact();
