c238a5e5d448c29413299fba377c64d4877c1d58
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-07-02 14:34:46 +0200

    Rename class and add MethodArgumentNotValidException exceptionhandler for DTO Validation exceptions
    Signed-off-by: Adam El Zahiri <adamxelz@gmail.com>

---
+++ src/main/java/io/github/adampyramide/BlogAPI/exception/GlobalExceptionHandler.java
@@ -0,0 +1,54 @@
+package io.github.adampyramide.BlogAPI.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<Object> handleApiRequestException(CustomException e) {
+        HttpStatus httpStatus = e.getHttpStatus();
+
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+                e.getMessage(),
+                httpStatus,
+                ZonedDateTime.now(ZoneId.of("Z"))
+        );
+
+        return new ResponseEntity<>(exceptionResponse, httpStatus);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        List<Map<String, String>> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fieldError -> {
+                    Map<String, String> error = new HashMap<>();
+                    error.put("field", fieldError.getField());
+                    error.put("message", fieldError.getDefaultMessage());
+                    return error;
+                })
+                .collect(Collectors.toList());
+
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("timestamp", ZonedDateTime.now(ZoneId.of("Z")));
+        responseBody.put("status", HttpStatus.BAD_REQUEST.value());
+        responseBody.put("errors", errors);
+
+        return new ResponseEntity<>(responseBody, HttpStatus.BAD_REQUEST);
+    }
+
+}
