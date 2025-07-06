9c9830164d7df0c5407c7777e924b8e0ea1382e2
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-07-06 11:47:57 +0200

    Update api-docs.json
    Signed-off-by: Adam El Zahiri <adamxelz@gmail.com>

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://adampyramide.github.io/BlogAPI/swagger.json",
+    url: "https://adampyramide.github.io/BlogAPI/api-docs.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
