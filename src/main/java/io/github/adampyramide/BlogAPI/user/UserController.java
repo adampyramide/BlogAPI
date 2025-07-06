a97f02e381fd2f92bc6223a7a2d7b76589dfc2c0
Adam El Zahiri <adamxelz@gmail.com>
2025-07-06 11:44:44 +0200

    Add Swagger annotations for API Documentation
    Signed-off-by: Adam El Zahiri <adamxelz@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/user/UserController.java
+++ src/main/java/io/github/adampyramide/BlogAPI/user/UserController.java
@@ -2,12 +2,17 @@ package io.github.adampyramide.BlogAPI.user;
 
 import io.github.adampyramide.BlogAPI.blogpost.BlogPostResponseDTO;
 import io.github.adampyramide.BlogAPI.blogpost.BlogPostService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Users", description = "User-specific operations")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -21,11 +26,25 @@ public class UserController {
         this.blogPostService = blogPostService;
     }
 
+    @Operation(
+            summary = "Get a user by id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "User found"),
+                    @ApiResponse(responseCode = "404", description = "User not found")
+            }
+    )
     @GetMapping("/{id}")
     public ResponseEntity<PublicUserDTO> getUserById(@PathVariable int id) {
         return ResponseEntity.status(HttpStatus.OK).body(service.getUserById(id));
     }
 
+    @Operation(
+            summary = "Get all posts created by a user with specified id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "User found"),
+                    @ApiResponse(responseCode = "404", description = "User not found")
+            }
+    )
     @GetMapping("/{id}/posts")
     public ResponseEntity<List<BlogPostResponseDTO>> getPostsByUserId(@PathVariable int id) {
         return ResponseEntity.status(HttpStatus.OK).body(blogPostService.getPostsByUserId(id));
