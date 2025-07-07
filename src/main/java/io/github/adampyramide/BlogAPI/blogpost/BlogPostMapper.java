40bca26de7c93283ae6a26269e7bca05bc03d1a7
Adam El Zahiri <adamxelz@gmail.com>
2025-07-07 18:01:38 +0200

    Ignore fields for entity not present in DTOs
    Signed-off-by: Adam El Zahiri <adamxelz@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/blogpost/BlogPostMapper.java
+++ src/main/java/io/github/adampyramide/BlogAPI/blogpost/BlogPostMapper.java
@@ -2,15 +2,22 @@ package io.github.adampyramide.BlogAPI.blogpost;
 
 import io.github.adampyramide.BlogAPI.user.UserMapper;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring", uses = {UserMapper.class})
 public interface BlogPostMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createTime", ignore = true)
+    @Mapping(target = "author", ignore = true)
     BlogPost toEntity(BlogPostRequestDTO dto);
 
     BlogPostResponseDTO toResponseDTO(BlogPost blogPost);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createTime", ignore = true)
+    @Mapping(target = "author", ignore = true)
     void updateBlogPostFromDto(BlogPostRequestDTO dto, @MappingTarget BlogPost entity);
 
 }
