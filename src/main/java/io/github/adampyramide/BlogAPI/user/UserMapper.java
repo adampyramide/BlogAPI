3df38e98fb8c8df5d1436be87a521311d8e5ecd1
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-07-07 18:01:38 +0200

    Ignore fields for entity not present in DTOs
    Signed-off-by: Adam El Zahiri <adamxelz@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/user/UserMapper.java
+++ src/main/java/io/github/adampyramide/BlogAPI/user/UserMapper.java
@@ -1,10 +1,12 @@
 package io.github.adampyramide.BlogAPI.user;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+    @Mapping(target = "id", ignore = true)
     User authDTOToEntity(AuthUserDTO dto);
 
     PublicUserDTO toPublicDTO(User user);
