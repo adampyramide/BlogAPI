5e9ad2eb8e1055d7671fd7e8056b2a367a8e33eb
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-07-11 10:41:57 +0200

    Add findAll blogposts with paging
    Signed-off-by: Adam El Zahiri <adamxelz@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/blogpost/BlogPostRepository.java
+++ src/main/java/io/github/adampyramide/BlogAPI/blogpost/BlogPostRepository.java
@@ -1,6 +1,8 @@
 package io.github.adampyramide.BlogAPI.blogpost;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Repository;
 
 import java.util.Arrays;
@@ -8,6 +10,8 @@ import java.util.List;
 
 @Repository
 public interface BlogPostRepository extends JpaRepository<BlogPost, Integer> {
+
+    Page<BlogPost> findAll(PageableDefault pageable);
 
     List<BlogPost> findAllByAuthor_Id(int userId);
 
