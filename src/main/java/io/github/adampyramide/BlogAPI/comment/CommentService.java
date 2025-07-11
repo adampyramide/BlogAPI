b9949bc0566069b88d980e69f743f67515708c9c
Adam El Zahiri <adamxelz@gmail.com>
2025-07-11 11:16:48 +0200

    Set up comment package
    Signed-off-by: Adam El Zahiri <adamxelz@gmail.com>

---
+++ src/main/java/io/github/adampyramide/BlogAPI/comment/CommentService.java
@@ -0,0 +1,13 @@
+package io.github.adampyramide.BlogAPI.comment;
+
+public class CommentService {
+
+    final CommentRepository repo;
+
+    public CommentService(CommentRepository repo) {
+        this.repo = repo;
+    }
+
+
+
+}
