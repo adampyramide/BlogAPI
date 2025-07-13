35ebc21003eb9f21aaedc2cf948dca3fbf555f27
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-07-14 01:39:59 +0300

    Remove unused imports
    Signed-off-by: Adam El Zahiri <adamxelz@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/blogpost/BlogPostRepository.java
+++ src/main/java/io/github/adampyramide/BlogAPI/blogpost/BlogPostRepository.java
@@ -1,12 +1,8 @@
 package io.github.adampyramide.BlogAPI.blogpost;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Repository;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Repository
