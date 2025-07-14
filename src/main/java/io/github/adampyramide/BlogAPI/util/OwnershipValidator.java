8fba269490de8ce361874e03609d6da5e5661f1f
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-07-14 20:12:08 +0300

    feat: add OwnershipValidator utility for author authorization checks
    Signed-off-by: Adam El Zahiri <adamxelz@gmail.com>

---
+++ src/main/java/io/github/adampyramide/BlogAPI/util/OwnershipValidator.java
@@ -0,0 +1,17 @@
+package io.github.adampyramide.BlogAPI.util;
+
+import io.github.adampyramide.BlogAPI.exception.CustomException;
+import io.github.adampyramide.BlogAPI.user.User;
+import org.springframework.http.HttpStatus;
+
+public class OwnershipValidator {
+
+    public static void authorizeAuthor(User author, User currentUser, String resourceName) {
+        if (!author.getId().equals(currentUser.getId())) {
+            throw new CustomException(
+                    "You are not authorized to modify this " + resourceName.toLowerCase(), HttpStatus.UNAUTHORIZED
+            );
+        }
+    }
+
+}
