b3f4b0fdd0e2fd825c79c9b939658dc638c495cb
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-07-15 21:31:08 +0300

    Change likeCount / dislikeCount field datatype from int to long
    Signed-off-by: Adam El Zahiri <adamxelz@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/blogpost/BlogPostResponseDTO.java
+++ src/main/java/io/github/adampyramide/BlogAPI/blogpost/BlogPostResponseDTO.java
@@ -11,7 +11,7 @@ public record BlogPostResponseDTO(
         String title,
         String body,
         LocalDateTime createTime,
-        int likeCount,
-        int dislikeCount
+        long likeCount,
+        long dislikeCount
 
 ) {}
