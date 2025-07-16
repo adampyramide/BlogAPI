5ca3a9617daa2b075374ed6b5d4d87ca0bd0b0ff
Adam El Zahiri <adamxelz@gmail.com>
2025-07-16 23:21:00 +0300

    Change record to class and add boolean hasReplies field
    Signed-off-by: Adam El Zahiri <adamxelz@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/comment/CommentResponseDTO.java
+++ src/main/java/io/github/adampyramide/BlogAPI/comment/CommentResponseDTO.java
@@ -1,13 +1,23 @@
 package io.github.adampyramide.BlogAPI.comment;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.github.adampyramide.BlogAPI.user.PublicUserDTO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-public record CommentResponseDTO(
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CommentResponseDTO {
 
-        Long id,
-        Long postId,
-        Long parentCommentId,
-        PublicUserDTO author,
-        String body
-        
-) {}
+    private Long id;
+    private Long postId;
+    private Long parentCommentId;
+    private PublicUserDTO author;
+    private String body;
+    private boolean hasReplies;
+
+}
