52ebbcac1e1e9ba3678e1555fd98680e4c7d7efe
Adam El Zahiri <adamxelz@gmail.com>
2025-07-18 00:13:03 +0300

    Add documentation & ResponseEntities to endpoints
    Signed-off-by: Adam El Zahiri <adamxelz@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/reaction/ReactionController.java
+++ src/main/java/io/github/adampyramide/BlogAPI/reaction/ReactionController.java
@@ -1,5 +1,11 @@
 package io.github.adampyramide.BlogAPI.reaction;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,19 +20,45 @@ public class ReactionController {
         this.service = service;
     }
 
+    @Operation(
+            summary = "Get reactions for blogpost",
+            description = "Returns a list of users and their reaction type for the specified blogpost. Optional filter by reaction type (LIKE or DISLIKE).",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Users and their reaction type returned"),
+                    @ApiResponse(responseCode = "404", description = "Post not found", content = @Content())
+            }
+    )
     @GetMapping
-    public List<ReactionResponseDTO> getReactionsByPostId(@PathVariable Long postId, @RequestParam(required = false) ReactionType reactionType) {
-        return service.getReactionsByPostId(postId, reactionType);
+    public ResponseEntity<List<ReactionResponseDTO>> getReactionsByPostId(@PathVariable Long postId, @RequestParam(required = false) ReactionType reactionType) {
+        return ResponseEntity.ok(service.getReactionsByPostId(postId, reactionType));
     }
 
+    @Operation(
+            summary = "Add reaction",
+            description = "Adds a reaction (LIKE or DISLIKE) to a blog post by the current user. Overwrites any existing reaction.",
+    responses = {
+                    @ApiResponse(responseCode = "201", description = "Reaction added to blogpost"),
+                    @ApiResponse(responseCode = "404", description = "Post not found", content = @Content())
+            }
+    )
     @PostMapping
-    public void addReactionByPostId(@PathVariable Long postId, @RequestBody ReactionRequestDTO reactionDTO) {
+    public ResponseEntity<Void> addReactionByPostId(@PathVariable Long postId, @RequestBody ReactionRequestDTO reactionDTO) {
         service.addReactionByPostId(postId, reactionDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(
+            summary = "Remove reaction",
+            description = "Removes the current user's reaction (LIKE or DISLIKE) from the blog post.",
+    responses = {
+                    @ApiResponse(responseCode = "204", description = "Reaction removed from blogpost"),
+                    @ApiResponse(responseCode = "404", description = "Post not found", content = @Content())
+            }
+    )
     @DeleteMapping
-    public void deleteReactionByPostId(@PathVariable Long postId) {
+    public ResponseEntity<Void> deleteReactionByPostId(@PathVariable Long postId) {
         service.deleteReactionByPostId(postId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 }
