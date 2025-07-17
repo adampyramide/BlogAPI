09185805ddd14468ce90bb9603dc0fc929022693
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-07-17 20:47:14 +0300

    Change record to class to implement reactions logic (likeCount,dislikeCount)
    Signed-off-by: Adam El Zahiri <adamxelz@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/blogpost/BlogPostResponseDTO.java
+++ src/main/java/io/github/adampyramide/BlogAPI/blogpost/BlogPostResponseDTO.java
@@ -1,17 +1,25 @@
 package io.github.adampyramide.BlogAPI.blogpost;
 
 import io.github.adampyramide.BlogAPI.user.PublicUserDTO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-public record BlogPostResponseDTO(
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BlogPostResponseDTO {
+    
+    private Long id;
+    private PublicUserDTO author;
+    private String title;
+    private String body;
+    private LocalDateTime createTime;
+    private long likeCount;
+    private long dislikeCount;
 
-        Long id,
-        PublicUserDTO author,
-        String title,
-        String body,
-        LocalDateTime createTime,
-        long likeCount,
-        long dislikeCount
-
-) {}
+}
