e0bac12940106e9a89d9e494b909a73a29aa3a1d
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-07-23 21:59:49 +0300

    Add javadoc to authorizeAuthor method
    Signed-off-by: Adam El Zahiri <adamxelz@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/util/OwnershipValidator.java
+++ src/main/java/io/github/adampyramide/BlogAPI/util/OwnershipValidator.java
@@ -6,6 +6,14 @@ import org.springframework.http.HttpStatus;
 
 public class OwnershipValidator {
 
+    /**
+     * Validates whether the current user is the owner of a resource.
+     *
+     * @param author        the user who owns the resource
+     * @param currentUser   the user performing the action
+     * @param resourceName  a readable name for the resource (e.g., "comment", "post")
+     * @throws CustomException if the current user is not the author
+     */
     public static void authorizeAuthor(User author, User currentUser, String resourceName) {
         if (!author.getId().equals(currentUser.getId())) {
             throw new CustomException(
