aed40fc9abb4d3fac36d6996ff007d46e5aa83e0
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-07-28 12:04:44 +0300

    Add initial UpdateUserRequest for PATCH operation
    Signed-off-by: Adam El Zahiri <adamelzahiri.dev@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/user/UpdateUserRequest.java
+++ src/main/java/io/github/adampyramide/BlogAPI/user/UpdateUserRequest.java
@@ -1,7 +1,11 @@
 package io.github.adampyramide.BlogAPI.user;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public record UpdateUserRequest(
 
-
+        String username,
+//        MultipartFile profilePicture,
+        String description
 
 ) {}
