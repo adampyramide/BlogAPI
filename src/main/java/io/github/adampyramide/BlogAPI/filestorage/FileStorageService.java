c34d8ea1f974f5c3dba4ea9f0fb066135d5079a6
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-07-31 00:12:06 +0300

    chore: Remove unused load method and in delete method rename parameter fileUrl to fileId
    Signed-off-by: Adam El Zahiri <adamelzahiri.dev@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/filestorage/FileStorageService.java
+++ src/main/java/io/github/adampyramide/BlogAPI/filestorage/FileStorageService.java
@@ -1,12 +1,10 @@
 package io.github.adampyramide.BlogAPI.filestorage;
 
-import jakarta.annotation.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileStorageService  {
 
-    String save(MultipartFile file, String folder);
-    void delete(String fileUrl);
-    Resource load(String fileUrl);
+    String save(MultipartFile file, String folder, FileValidationRule validationRule);
+    void delete(String fileId);
 
 }
