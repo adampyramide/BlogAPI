c4e8b401d7f479fd811d0806c45f2d0591f5c25d
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-07-31 23:46:37 +0300

    refactor: rename BlogPostFetcher to BlogPostQueryService and annotate with service instead of component
    Signed-off-by: Adam El Zahiri <adamelzahiri.dev@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/blogpost/BlogPostQueryService.java
+++ src/main/java/io/github/adampyramide/BlogAPI/blogpost/BlogPostQueryService.java
@@ -4,10 +4,11 @@ import io.github.adampyramide.BlogAPI.error.ApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 @RequiredArgsConstructor
-public class BlogPostFetcher {
+public class BlogPostQueryService {
 
     private final BlogPostRepository repo;
 
