8c779cc6cd61e680cdecc0421c7615d93be002a3
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-07-31 23:47:01 +0300

    feat: init UserQueryService for user look up
    Signed-off-by: Adam El Zahiri <adamelzahiri.dev@gmail.com>

---
+++ src/main/java/io/github/adampyramide/BlogAPI/user/UserQueryService.java
@@ -0,0 +1,23 @@
+package io.github.adampyramide.BlogAPI.user;
+
+import io.github.adampyramide.BlogAPI.error.ApiException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserQueryService {
+
+    private final UserRepository repo;
+
+    public User getUserOrThrow(Long id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new ApiException(
+                        HttpStatus.NOT_FOUND,
+                        "USER_NOT_FOUND",
+                        "User with ID " + id + " not found."
+                ));
+    }
+
+}
