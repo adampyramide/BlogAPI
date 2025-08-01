7f6ab259b00a1647a53bbcf9e7e8964f566d695e
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-08-01 19:30:03 +0300

    refactor: rename class to UserUtils and move to user package. rename authorizeAuthor method to validateOwnership
    Signed-off-by: Adam El Zahiri <adamelzahiri.dev@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/user/UserUtils.java
+++ src/main/java/io/github/adampyramide/BlogAPI/user/UserUtils.java
@@ -1,20 +1,19 @@
-package io.github.adampyramide.BlogAPI.util;
+package io.github.adampyramide.BlogAPI.user;
 
 import io.github.adampyramide.BlogAPI.error.ApiException;
-import io.github.adampyramide.BlogAPI.user.User;
 import org.springframework.http.HttpStatus;
 
-public class OwnershipValidator {
+public class UserUtils {
 
     /**
-     * Validates whether the current user is the owner of a resource.
+     * Validates whether the current user is the owner of a resource. If not, it will throw an API exception.
      *
      * @param author        the user who owns the resource
      * @param user   the user performing the action
-     * @param resourceName  a readable name for the resource (e.g., "comment", "post")
+     * @param resourceName  a readable name for the resource (e.g., "comment", "blogpost")
      * @throws ApiException if the current user is not the author
      */
-    public static void authorizeAuthor(User author, User user, String resourceName) {
+    public static void validateOwnership(User author, User user, String resourceName) {
         if (!author.getId().equals(user.getId())) {
             throw new ApiException(
                     HttpStatus.FORBIDDEN,
