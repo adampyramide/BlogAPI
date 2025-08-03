940b3c9f5737ea87f1d0cafa1b8c1150c3a1f894
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-08-04 01:23:45 +0300

    chore: remove unused imports
    Signed-off-by: Adam El Zahiri <adamelzahiri.dev@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/blogpost/BlogPostQueryService.java
+++ src/main/java/io/github/adampyramide/BlogAPI/blogpost/BlogPostQueryService.java
@@ -3,7 +3,6 @@ package io.github.adampyramide.BlogAPI.blogpost;
 import io.github.adampyramide.BlogAPI.error.ApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
