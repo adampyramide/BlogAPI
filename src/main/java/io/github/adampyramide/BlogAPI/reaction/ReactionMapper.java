22e43523ace66f4af7ef703c2f2b9fe7b367ef7b
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-08-04 01:29:25 +0300

    fix: add UserMapper as uses in @Mapper to enrich embedded userResponse within reactionResponse
    Signed-off-by: Adam El Zahiri <adamelzahiri.dev@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/reaction/ReactionMapper.java
+++ src/main/java/io/github/adampyramide/BlogAPI/reaction/ReactionMapper.java
@@ -1,9 +1,13 @@
 package io.github.adampyramide.BlogAPI.reaction;
 
 import io.github.adampyramide.BlogAPI.reaction.dto.ReactionResponse;
+import io.github.adampyramide.BlogAPI.user.UserMapper;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+@Mapper(
+        componentModel = "spring",
+        uses = UserMapper.class
+)
 public interface ReactionMapper {
 
     ReactionResponse toResponse(Reaction entity);
