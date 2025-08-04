9977a92b8ce1238451e99873c6fdf59b9143f876
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-08-05 00:04:09 +0300

    feat: add email field
    Signed-off-by: Adam El Zahiri <adamelzahiri.dev@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/auth/AuthRequest.java
+++ src/main/java/io/github/adampyramide/BlogAPI/auth/AuthRequest.java
@@ -1,15 +1,20 @@
 package io.github.adampyramide.BlogAPI.auth;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record AuthRequest(
 
         @NotBlank(message = "Username must not be blank")
+        @Size(max = 20, message = "Username must be less than 20 characters")
         String username,
 
         @NotBlank(message = "Password must not be blank")
         @Size(min = 6, max = 128, message = "Password must be between 6 and 128 characters")
-        String password
+        String password,
+
+        @Email(message = "Invalid email format")
+        String email
 
 ) {}
