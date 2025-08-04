27c5d9ebf299e8e23f3d5c0d33b9fb22762bc923
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-08-05 00:05:17 +0300

    feat: add fields email, displayName, dateOfBirth and gender. improve validation
    Signed-off-by: Adam El Zahiri <adamelzahiri.dev@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/user/User.java
+++ src/main/java/io/github/adampyramide/BlogAPI/user/User.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
@@ -21,20 +22,49 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false, unique = true, length = 20)
     private String username;
 
     @Column(nullable = false)
     private String password;
 
-    @Column(updatable = false)
-    @CreationTimestamp
-    private LocalDateTime createdAt;
+    @Column(unique = true, length = 320)
+    private String email;
 
     @Column
     private String avatarId;
 
-    @Column
+    @Column(length = 20)
+    private String displayName;
+
+    @Column(length = 500)
     private String description;
+
+    @Column
+    private LocalDate dateOfBirth;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Gender gender = Gender.UNSPECIFIED;
+
+    @PrePersist
+    public void prePersist() {
+        preChange();
+
+        if (gender == null) gender = Gender.UNSPECIFIED;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        preChange();
+    }
+
+    private void preChange() {
+        if (email != null) email = email.toLowerCase();
+    }
 
 }
--- src/main/java/io/github/adampyramide/BlogAPI/user/dto/UpdateUserRequest.java
+++ src/main/java/io/github/adampyramide/BlogAPI/user/dto/UpdateUserRequest.java
@@ -1,15 +1,36 @@
 package io.github.adampyramide.BlogAPI.user.dto;
 
+import io.github.adampyramide.BlogAPI.user.Gender;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDate;
 
 @Builder
 public record UpdateUserRequest(
 
+        @Size(min = 1, max = 20, message = "Username must be between 1 and 20 characters")
         String username,
+
+        @Email(message = "Invalid email format")
+        String email,
+
         MultipartFile avatarImage,
+
+        Boolean removeAvatar,
+
+        @Size(min = 1, max = 20, message = "Display name must be between 1 and 20 characters")
+        String displayName,
+
+        @Size(max = 500, message = "Description must be less than 500 characters")
         String description,
 
-        Boolean removeAvatar
+        @Past(message = "Date of birt must be in the past")
+        LocalDate dateOfBirth,
+
+        Gender gender
 
 ) {}
